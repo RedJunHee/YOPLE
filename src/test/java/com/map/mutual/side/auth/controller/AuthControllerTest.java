f2a63682d5688a9da57c0fa4ebc94960d4e32941
조준희 <46863381+RedJunHee@users.noreply.github.com>
2022-04-13 14:17:01 +0900

    Auth 테스트 코드 작성.

--- src/test/java/com/map/mutual/side/auth/controller/AuthControllerTest.java
+++ src/test/java/com/map/mutual/side/auth/controller/AuthControllerTest.java
@@ -1,50 +1,98 @@
 package com.map.mutual.side.auth.controller;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.map.mutual.side.auth.model.dto.SMSAuthReqeustDto;
-import lombok.extern.log4j.Log4j2;
-import org.junit.jupiter.api.Disabled;
+import com.map.mutual.side.AbstractControllerTest;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
-import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.ActiveProfiles;
-import org.springframework.test.context.junit.jupiter.SpringExtension;
-import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.transaction.annotation.Transactional;
+import static org.hamcrest.Matchers.is;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-import javax.transaction.Transactional;
-
-
-@SpringBootTest
-@ActiveProfiles("local")
-@AutoConfigureMockMvc
-@Log4j2
-class AuthControllerTest {
+class AuthControllerTest extends AbstractControllerTest {
 
     @Autowired
-    private MockMvc mockMvc;
+    public AuthController authController;
 
+    @Override
+    protected Object object() {
+        return authController;
+    }
 
 
     @Test
+    @DisplayName("sms인증번호요청_폰번호_길이초과")
     @Transactional
-    @DisplayName("가입요청 Validation 테스트")
-    void smsAuthenticationRequest() throws Exception {
-        ObjectMapper objectMapper = new ObjectMapper();
-        SMSAuthReqeustDto smsAuthReqeustDto = new SMSAuthReqeustDto(
-                "111",
-                "abcadsf",
-                "1515151"
-        );
-        mockMvc.perform(MockMvcRequestBuilders
-                .post("/auth/sms-authentication-request")
-                .contentType("application/json")
-                .content(objectMapper.writeValueAsString(smsAuthReqeustDto)));
-//                .andExpect(result -> log.info("Result : {}", result));
+    public void sms인증번호요청_폰번호_길이초과() throws Exception {
+
+        String body = "{\"phone\":\"010123412834\",\"duid\":\"test_duid\" }";
+
+            mockMvc.perform(post("/auth/sms-authentication-request")
+                            .content(body).contentType(this.contentType))
+                    .andExpect(status().isOk())
+                    .andExpect(jsonPath("$.meta.code",is(400)));
+
     }
+
+    @Test
+    @DisplayName("sms인증번호요청_폰번호_길이부족")
+    @Transactional
+    public void sms인증번호요청_폰번호_길이부족() throws Exception {
+
+        String body = "{\"phone\":\"010123834\",\"duid\":\"test_duid\" }";
+
+        mockMvc.perform(post("/auth/sms-authentication-request")
+                        .content(body).contentType(this.contentType))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.meta.code",is(400)));
+
+    }
+
+    @Test
+    @DisplayName("sms인증번호요청_폰번호_문자포함")
+    @Transactional
+    public void sms인증번호요청_폰번호_문자포함() throws Exception {
+
+        String body = "{\"phone\":\"010ㅁㅇㅉㄸ1234\",\"duid\":\"test_duid\" }";
+
+        mockMvc.perform(post("/auth/sms-authentication-request")
+                        .content(body).contentType(this.contentType))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.meta.code",is(400)));
+
+    }
+
+    @Test
+    @DisplayName("sms인증번호요청_폰번호_010시작안함")
+    @Transactional
+    public void sms인증번호요청_폰번호_010시작안함() throws Exception {
+
+        String body = "{\"phone\":\"01312341283\",\"duid\":\"test_duid\" }";
+
+        mockMvc.perform(post("/auth/sms-authentication-request")
+                        .content(body).contentType(this.contentType))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.meta.code",is(400)));
+
+    }
+
+    @Test
+    @DisplayName("sms인증번호요청_정상")
+    @Transactional
+    public void sms인증번호요청_정상() throws Exception {
+
+        String body = "{\"phone\":\"01027090787\",\"duid\":\"test_duid\" }";
+
+        mockMvc.perform(post("/auth/sms-authentication-request")
+                        .content(body).contentType(this.contentType))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.meta.code",is(200)));
+
+    }
+
+
+
+
 }
+트

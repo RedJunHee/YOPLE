cadeb41445ca2bd303b09ce36a793a01acdc9e34
조준희 <46863381+RedJunHee@users.noreply.github.com>
2022-04-14 16:03:25 +0900

    API_LOG SUID 추가.

--- src/main/java/com/map/mutual/side/common/entity/ApiLog.java
+++ src/main/java/com/map/mutual/side/common/entity/ApiLog.java
@@ -23,6 +23,10 @@ public class ApiLog extends CreateDtEntity {
     @Column(name="SEQ", columnDefinition = "BIGINT")
     private Long seq;
 
+    // TODO: 2022-04-14  suid null 허용 안함으로 변경해야함.
+    @Column(name="SUID", nullable = true, length = 18, columnDefinition = "VARCHAR(18)")
+    private String suid;
+
     @Column(name="API_NAME", nullable = false, length = 100, columnDefinition = "VARCHAR(100)")
     private  String apiName;
 

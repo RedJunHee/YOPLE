4ed7f9c33bd8dbf7adb4eff2ee4ffcc3bcf894ce
jojunhui <redjoon10@gmail.com>
2022-04-27 21:57:30 +0900

    1. 이모지 알림 쿼리 수정 2. 월드 참여자 조회 - 차단 필터링 추가

--- src/main/java/com/map/mutual/side/auth/repository/UserBlockLogRepo.java
+++ src/main/java/com/map/mutual/side/auth/repository/UserBlockLogRepo.java
@@ -5,6 +5,8 @@ import com.map.mutual.side.auth.repository.dsl.UserBlockLogRepoDSL;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 /**
  * Class       : UserBlockLogRepo
  * Author      : 조 준 희
@@ -13,5 +15,18 @@ import org.springframework.stereotype.Repository;
  */
 @Repository
 public interface UserBlockLogRepo extends JpaRepository<UserBlockLogEntity, Long> , UserBlockLogRepoDSL {
-    boolean existsByUserSuidAndBlockSuidAndAndIsBlocking(String userSuid, String blockSuid, String isBlocking);
+    /**
+     * Description : 사용자 차단 정보가 존재하는지 여부.
+     * Name        : existsByUserSuidAndBlockSuidAndIsBlocking
+     * Author      : 조 준 희
+     * History     : [2022/04/27] - 조 준 희 - Create
+     */
+    boolean existsByUserSuidAndBlockSuidAndIsBlocking(String userSuid, String blockSuid, String isBlocking);
+    /**
+     * Description : 사용자가 현재 차단 중인 차단 리스트 조회
+     * Name        : findByUserSuidAndIsBlocking
+     * Author      : 조 준 희
+     * History     : [2022/04/27] - 조 준 희 - Create
+     */
+    List<UserBlockLogEntity> findByUserSuidAndIsBlocking(String userSuid,  String isBlocking);
 }
